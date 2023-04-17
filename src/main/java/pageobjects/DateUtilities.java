0278cef3c175663e3e29b79d10aa62c2b7284dd5
sangeethaann <sangeetha.ann1995@gmail.com>
2023-04-17 10:37:44 +0300

    Added the LoginTest and DateUtilities classes....

---
+++ src/main/java/pageobjects/DateUtilities.java
@@ -0,0 +1,16 @@
+package com.finalmainproject.Utilties;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtilities {
+	
+	public static String getCurrentDateStr()
+	{
+		String dateName=new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		return dateName;
+		
+		
+	}
+
+}
